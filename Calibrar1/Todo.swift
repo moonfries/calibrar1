b459aa3bd80532f340d7f8a1416131676916958e
Jake <jakearanas@gmail.com>
2022-01-18 12:55:27 +0800

    first commit

---
+++ Calibrar1/Todo.swift
@@ -0,0 +1,8 @@
+//
+//  Todo.swift
+//  Calibrar1
+//
+//  Created by Jake Aranas on 1/18/22.
+//
+
+import Foundation
---
+++ Calibrar1/TodoService.swift
@@ -0,0 +1,8 @@
+//
+//  TodoService.swift
+//  Calibrar1
+//
+//  Created by Jake Aranas on 1/18/22.
+//
+
+import Foundation
